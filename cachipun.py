d7c8820547a03c6f3898477e335612dfd32515f7
lizatamara <lizaa1609@gmail.com>
2024-02-27 23:27:59 -0300

    agrego cachipun.py

---
+++ cachipun.py
@@ -0,0 +1,27 @@
+import random
+
+jugador = input("Ingresa piedra, papel o tijera: ")
+
+opciones = ['piedra', 'papel', 'tijera']
+
+computador = (random.choice(opciones))
+
+
+if jugador == 'piedra' or jugador == 'tijera' or jugador == 'papel':
+    
+    print(f"Tu jugaste: {jugador}")
+    print(f"El computador jugó: {computador}")
+    if jugador == computador:
+        print("Es un empate")
+    elif jugador == 'piedra' and computador == 'tijera':
+        print ("Ganaste")
+    elif jugador == 'piedra' and computador == 'papel':
+        print ("Perdiste")
+    elif jugador == 'tijera' and computador == 'papel':
+        print ("Ganaste")
+    elif jugador == 'papel' and computador == 'piedra':
+        print ("Ganaste")
+    else:
+        print("Perdiste")
+else:
+    print('Argumento inválido: Debe ser piedra, papel o tijera')
